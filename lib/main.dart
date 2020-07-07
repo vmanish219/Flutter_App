0f5215ea9e3646d376b33a9376d182c74b119d2e
manish verma <vmanish219@gmail.com>
2020-07-07 11:30:23 +0530

    commit 2

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  int qIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      qIndex++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = ["Question 1", "Question 2", "Question 3", "Question 4"];
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("My First App"),
+          ),
+          body: Column(
+            children: [
+              Text(questions[qIndex]),
+              RaisedButton(child: Text("OP 1"), onPressed: answerQuestion),
+              RaisedButton(child: Text("OP 2"), onPressed: answerQuestion),
+              RaisedButton(child: Text("OP 3"), onPressed: answerQuestion),
+            ],
+          )),
+    );
+  }
+}
